401227ad800de701d0a79a8e03e69725e93c76d2
Carmen <carmen.casulli33@gmail.com>
2024-06-01 12:05:24 +0200

    cambiato leggermente fermat

--- src/fermat/fermat.cpp
+++ src/fermat/fermat.cpp
@@ -4,16 +4,20 @@
 
 std::pair<mpz_class, mpz_class> fermat(mpz_class n){
     //std::cerr << n << std::endl;
-    mpz_class base = sqrt(n), h = base*base - n, i = 0;
-    mpz_class x, y = 0, nHalves = n/2, twoBase = 2*base;
+    mpz_class base = sqrt(n), h = base*base - n;
+    //mpz_class i = 0;
+    mpz_class x = base, y = 0, nHalves = n/2;
+    //mpz_class twoBase = 2*base;
 
     while(y < nHalves){
-        h = h + twoBase + 2*i + 1;
-        i = i+1;
+        //h = h + twoBase + 2*i + 1;
+        //i = i+1;
+        h = h + (x<<1) + 1;
+        x++;
         y = sqrt(h);
 
         if(y*y == h){
-            x = base + i;
+            //x = base + i;
             break;
         }
     }
