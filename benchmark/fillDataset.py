1799dda8b8fa94b59409695a242c3c76e9ba3ef6
Carmen <carmen.casulli33@gmail.com>
2024-06-13 23:43:22 +0200

    creato primo dataset per benchmarking

---
+++ benchmark/fillDataset.py
@@ -0,0 +1,29 @@
+from Cryptodome.Util.number import getPrime
+import json
+import math
+
+x = math.pow(256, 1/100)
+numbits = [int(pow(x, i)) for i in range(116)]
+numbits = [num for num in numbits if num > 8]
+
+
+f = open("dataset.txt", "w")
+
+last = 1
+for i, numbit in enumerate(numbits):
+    p = getPrime(numbit//2)
+    q = p
+    n = p*q
+
+    while q==p or n.bit_length() < numbit or n == last:
+        q = getPrime((numbit+1)//2)
+        n = p*q
+
+    #print(n)
+    last = n
+    line = ({'index': i, 'numbits (of n)': numbit, 'n': n, '(p, q)': (p, q)})
+    json.dump(line, f)
+    f.write('\n')
+
+
+f.close()
